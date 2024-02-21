59eb65f8e78a1132bbf4c5b9d9805c6da11a911c
David Choqueluque <dchoqueluque@gmail.com>
2024-02-21 17:03:06 -0500

    updated ecs script

--- david/build_docker_image_aws.sh
+++ david/build_docker_image_aws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t heart-dissease-prediction-api:latest . -f david/Dockerfile
+docker build -t heart-disease-api-ecr-repo:latest . -f david/Dockerfile
 docker images
-docker tag heart-dissease-prediction-api:latest 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo/heart-dissease-prediction-api:latest
+docker tag heart-disease-api-ecr-repo:latest 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo:latest
 
 # aws ecr  get-login --no-include-email --region us-east-1
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 656662427506.dkr.ecr.us-east-1.amazonaws.com
+aws --profile developer1 ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 656662427506.dkr.ecr.us-east-1.amazonaws.com
 # docker run -p 8001:8001 -e PORT=8001 heart-dissease-prediction-api
-docker push 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo/heart-dissease-prediction-api:latest
+docker push 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo:latests
