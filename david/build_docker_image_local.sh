e28d0a023db122dda841a233675782e8c47a6507
David Choqueluque <dchoqueluque@gmail.com>
2024-02-21 18:55:09 -0500

    updated ecs script run.sh

--- david/build_docker_image_local.sh
+++ david/build_docker_image_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t heart-dissease-prediction-api:latest .
+docker build -t heart-dissease-prediction-api:latest . -f david/Dockerfile
 docker run -p 8001:8001 -e PORT=8001 heart-dissease-prediction-api
