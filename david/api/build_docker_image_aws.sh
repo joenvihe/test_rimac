fbd9c998d57b6998ab084013a13f96b666e0486e
David Choqueluque <dchoqueluque@gmail.com>
2024-02-21 04:13:02 -0500

    updated cicd2

--- david/api/build_docker_image_aws.sh
+++ david/api/build_docker_image_aws.sh
@@ -7,5 +7,4 @@ docker tag heart-dissease-prediction-api:latest 656662427506.dkr.ecr.us-east-1.a
 # aws ecr  get-login --no-include-email --region us-east-1
 
 # docker run -p 8001:8001 -e PORT=8001 heart-dissease-prediction-api
-
 docker push 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo/heart-dissease-prediction-api:latest
