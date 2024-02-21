866ab6e64de80586a7e0d72b03cb43559fd946fd
David Choqueluque <dchoqueluque@gmail.com>
2024-02-21 17:05:08 -0500

    updated ecs script

--- david/build_docker_image_aws.sh
+++ david/build_docker_image_aws.sh
@@ -5,6 +5,6 @@ docker images
 docker tag heart-disease-api-ecr-repo:latest 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo:latest
 
 # aws ecr  get-login --no-include-email --region us-east-1
-aws --profile developer1 ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 656662427506.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 656662427506.dkr.ecr.us-east-1.amazonaws.com
 # docker run -p 8001:8001 -e PORT=8001 heart-dissease-prediction-api
 docker push 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo:latests
