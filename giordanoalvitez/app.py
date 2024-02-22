0672c4dc4bd832a3d5394af8bce0fe9ed6e777d1
gio <giomikecs17@gmail.com>
2024-02-21 22:34:23 -0500

    Feat(cloud build): MLOps using Github

---
+++ giordanoalvitez/app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+
+app = FastAPI()
+
+client = bigquery.Client()
+
+# Define a root `/` endpoint
+@app.get('/')
+def index():
+    return {'ok': True}
+
+
+@app.get('/predict')
+def predict():
+    return {'wait': 64}
+
+@app.get('/load_data')
+def load_datos():
+
+    table_id = "heartdisease-414803.heartdataset.heart_table"
+    
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+    )
+
+    uri = "gs://mybucket-heartdisease-20245/datos/heart.csv"
+    load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)
+
+    load_job.result()  
+
+    destination_table = client.get_table(table_id)
+    return {"data": destination_table.num_rows}
