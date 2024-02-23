f33683305fc59ee858a7f4d8e6f1105515ccbc49
David Choqueluque <dchoqueluque@gmail.com>
2024-02-23 00:25:17 -0500

    update aws cli ecs command

--- david/build_docker_image_aws.sh
+++ david/build_docker_image_aws.sh
@@ -8,3 +8,7 @@ docker tag heart-disease-api-ecr-repo:latest 656662427506.dkr.ecr.us-east-1.amaz
 # aws --profile developer1 ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 656662427506.dkr.ecr.us-east-1.amazonaws.com
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 656662427506.dkr.ecr.us-east-1.amazonaws.com
 docker push 656662427506.dkr.ecr.us-east-1.amazonaws.com/heart-disease-api-ecr-repo:latest
+
+# aws ecs update-service --cluster heart-dissease-app-ecs-cluster --service service3 --task-definition  task2 --force-new-deployment
+aws ecs update-service --cluster heart-dissease-app-ecs-cluster --service service3 --desired-count 0 
+aws ecs update-service --cluster heart-dissease-app-ecs-cluster --service service3 --desired-count 1
