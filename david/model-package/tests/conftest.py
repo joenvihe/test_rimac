6187fbfdaca9741a7b1e6d406094d7abcf94ce93
David Choqueluque <dchoqueluque@gmail.com>
2024-02-22 23:20:31 -0500

    updated ci.cd flow to update model

--- david/model-package/tests/conftest.py
+++ david/model-package/tests/conftest.py
@@ -1,9 +1,10 @@
 import pytest
 
 from classifier_model.config.core import config
-from classifier_model.processing.data_manager import load_dataset
+from classifier_model.processing.data_manager import load_dataset, load_from_s3
 
 
 @pytest.fixture()
 def sample_input_data():
-    return load_dataset(file_name=config.app_config.test_data_file)
+    # return load_dataset(file_name=config.app_config.test_data_file)
+    return load_from_s3(file_name=config.app_config.test_data_file)
